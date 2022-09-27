0183ec4fab9cb8e7022dbd183978d03d6384e93c
gongchengxiang <gongchengxiang@baidu.com>
2022-09-27 14:41:08 +0800

    test2 module

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ export default function () {
     console.log('git-test');
     const test2 = 'branch:test2';
     console.log(test2);
+    const testFn = () => {
+        return [1,2,3,4,5].map(e => {
+            return e + '-test2';
+        });
+    };
+    return testFn();
+    // const 
 }
